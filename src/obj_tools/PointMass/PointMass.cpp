82b857031fa4678a8b34a5b15cc65adecdf3e49f
alexgerasimov58rus <vintegewhitecool@yandex.ru>
2021-03-30 18:20:27 +0300

    added methods for collision

--- src/obj_tools/PointMass/PointMass.cpp
+++ src/obj_tools/PointMass/PointMass.cpp
@@ -10,9 +10,47 @@
 using namespace obj_tools;
 using namespace std;
 
+void PointMass::handleCollision(PointMass* p1, PointMass* p2, const float& dTime) {
+    Vector3D distance = p1->centerMass_ - p2->centerMass_;
+    Vector3D unitNormal = distance.normalize();
+
+    float vel1 = (float)p1->linearVel_.dot(unitNormal);
+    float vel2 = (float)p2->linearVel_.dot(unitNormal);
+
+    float averageCoeffE = (p1->coeffElasticity_ + p2->coeffElasticity_) / 2.0f;
+
+    float finalVel1 = ( (p1->mass_ - averageCoeffE * p2->mass_) * vel1 + (1 + averageCoeffE) * p2->mass_ * vel2) / (p1->mass_ + p2->mass_);
+    float finalVel2 = ( (p2->mass_ - averageCoeffE * p1->mass_) * vel2 + (1 + averageCoeffE) * p1->mass_ * vel1) / (p1->mass_ + p2->mass_);
+
+    p1->linearVel_ = (finalVel1 - vel1) * unitNormal + p1->linearVel_;
+    p2->linearVel_ = (finalVel2 - vel2) * unitNormal + p2->linearVel_;
+
+    Vector3D velNormalize1 = p1->linearVel_.normalize();
+    Vector3D velNormalize2 = p2->linearVel_.normalize();
+
+    p1->forces_ = Vector3D(
+        (float)fabs(p1->forces_.getX()) * velNormalize1.getX(), 
+        (float)fabs(p1->forces_.getY()) * velNormalize1.getY(),
+        (float)fabs(p1->forces_.getZ()) * velNormalize1.getZ());
+
+    p2->forces_ = Vector3D(
+        (float)fabs(p2->forces_.getX()) * velNormalize2.getX(),
+        (float)fabs(p2->forces_.getY()) * velNormalize2.getY(),
+        (float)fabs(p2->forces_.getZ()) * velNormalize2.getZ());
+}
+
 PointMass::PointMass(Mesh* mesh, const float& mass) {
     this->mesh_ = mesh;
     this->mass_ = mass;
+    this->coeffElasticity_ = 0.5f;
+}
+
+bool PointMass::hasCollision(const PointMass& point) const {
+    double distanceSquared = (centerMass_ - point.centerMass_).normSquared();
+    double minDistanceSquared = mesh_->getBoundingSphereRadius() + point.mesh_->getBoundingSphereRadius();
+    minDistanceSquared *= minDistanceSquared;
+
+    return distanceSquared < minDistanceSquared;
 }
 
 void PointMass::update(const float& dTime) {
--- src/obj_tools/PointMass/PointMass.hpp
+++ src/obj_tools/PointMass/PointMass.hpp
@@ -2,7 +2,6 @@
 
 #include "../tools_exp.hpp"
 #include "../Vector/Vector3D.hpp"
-#include "../Matrix/Matrix3x3.hpp"
 
 namespace obj_tools {
 
@@ -10,10 +9,16 @@ namespace obj_tools {
 
 	struct OBJ_TOOLS_EXPORT PointMass
 	{
+		static void handleCollision(PointMass* p1, PointMass* p2, const float& dTime);
+
 		PointMass(Mesh* mesh, const float& mass);
 		
 		void setForces  (const Vector3D& forces) { forces_ = forces;  }
 		void setLocation(const Vector3D& location) { centerMass_ = location; }
+		
+		void setCoeffElasticity(const float& coeff) { coeffElasticity_ = coeff; }
+
+		bool hasCollision(const PointMass& point) const;
 
 		void update(const float& dTime);
 		void render(void);
@@ -23,12 +28,11 @@ namespace obj_tools {
 	
 		Mesh* mesh_;
 		float mass_;
+		float coeffElasticity_;
 
 		Vector3D centerMass_;
 		Vector3D linearVel_;
 		Vector3D linearAcc_;
 		Vector3D forces_;
-
-		Matrix3x3 matrix_;
 	};
 };
