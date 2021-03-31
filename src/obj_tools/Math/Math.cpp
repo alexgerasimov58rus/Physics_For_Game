6fa5b86ef00dca85d110d12aaf3e41923c93fd39
alexgerasimov58rus <vintegewhitecool@yandex.ru>
2021-03-31 20:27:56 +0300

    added method for Inertia moments for some shapes

--- src/obj_tools/Math/Math.cpp
+++ src/obj_tools/Math/Math.cpp
@@ -9,3 +9,30 @@ double Math::degToRad(const double& deg) {
     return deg * PI / 180.0;
 }
 
+double Math::getInertiaMomentHomogeneousSphere(const float& r, const float& m) {
+    return 2 * (double)m * r * r / 5.0;
+}
+
+double Math::getInertiaMomentHollowSphere(const float& r, const float& m) {
+    return 2 * (double)m * r * r / 3.0;
+}
+
+double Math::getInertiaMomentHoopRelativeAxis(const float& r, const float& m) {
+    return (double)m * r * r;
+}
+
+double Math::getInertiaMomentHoopRelativeDiameter(const float& r, const float& m) {
+    return (double)m * r * r / 2.0;
+}
+
+double Math::getInertiaMomentHoopRelativeTangent(const float& r, const float& m) {
+    return 3 * (double)m * r * r / 2.0;
+}
+
+double Math::getInertiaMomentCylinderRelativeAxis(const float& r, const float& m) {
+    return (double)m * r * r / 2.0;
+}
+
+double Math::getInertiaMomentCylinderRelativeDiameter(const float& r, const float& l, const float& m) {
+    return (double)m * r * r / 4.0 + (double)m * l * l / 12.0;
+}
--- src/obj_tools/Math/Math.hpp
+++ src/obj_tools/Math/Math.hpp
@@ -7,5 +7,13 @@ namespace obj_tools {
     {
         static const double PI;
         static double degToRad(const double& deg);
+
+        static double getInertiaMomentHomogeneousSphere(const float& r, const float& m);
+        static double getInertiaMomentHollowSphere(const float& r, const float& m);
+        static double getInertiaMomentHoopRelativeAxis(const float& r, const float& m);
+        static double getInertiaMomentHoopRelativeDiameter(const float& r, const float& m);
+        static double getInertiaMomentHoopRelativeTangent(const float& r, const float& m);
+        static double getInertiaMomentCylinderRelativeAxis(const float& r, const float& m);
+        static double getInertiaMomentCylinderRelativeDiameter(const float& r, const float& l, const float& m);
     };
 };
